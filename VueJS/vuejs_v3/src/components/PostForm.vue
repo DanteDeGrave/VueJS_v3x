e833b6ad20bada793af33a56a134bbe1ce4589f8
Владимир Попов <vladimir.system@gmail.com>
2022-01-30 00:48:49 +0500

    1:07:27  https://youtu.be/XzLuMtDelGk?t=4047

---
+++ VueJS/vuejs_v3/src/components/PostForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <form @submit.prevent>
+    <h2>Создание поста</h2>
+    <my-input
+      placeholder="Название"
+      v-model="post.title"
+    />
+    <my-input
+      placeholder="Описание"
+      v-model="post.body"
+    />
+    <my-button
+      class="btn-component"
+      @click="createPost"
+    >
+      Создать
+    </my-button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: '',
+        body: '',
+      }
+    }
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit('create', this.post);
+      this.post = {
+        title: '',
+        body: '',
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.btn-component {
+  align-self: flex-end;
+  margin-top: 15px;
+}
+</style>
